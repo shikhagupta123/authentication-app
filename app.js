5bc4066a4e0078666e5fd441d8ff08f8bc5a4cfa
Shikha Gupta <gshikha129@gmail.com>
2022-09-28 08:23:27 +0530

    Oauth with google, new secret submissions

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -28,21 +30,55 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login"}),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  });
 app.get("/register", function(req, res) {
   res.render("register");
 });
@@ -51,13 +87,17 @@ app.get("/login", function(req, res) {
 });
 app.get("/secrets", function(req, res){
   console.log("in secrets");
-  if(req.isAuthenticated()){
-    console.log("authenticated");
-    res.render("secrets");
-  }else{
-    console.log("not authenticated");
-    res.redirect("/login");
-  }
+  User.find({"secret":{$ne:null}},function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        console.log(foundUsers);
+        res.render("secrets", {usersWithSecrets:foundUsers});
+      }else{
+      }
+    }
+  });
 });
 app.get("/logout", function(req, res){
   req.logout(function(err) {
@@ -65,6 +105,13 @@ app.get("/logout", function(req, res){
     res.redirect('/');
   });
 });
+app.get("/submit",function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+})
 app.post("/register", function(req, res) {
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
@@ -79,6 +126,7 @@ app.post("/register", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
+  console.log("post log in");
   const user = new User({
     username: req.body.username,
     password: req.body.password
@@ -88,12 +136,28 @@ app.post("/login", function(req, res) {
       console.log(err);
     }else{
       passport.authenticate("local")(req, res, function(){
+        console.log("authenticated");
         res.redirect("/secrets");
       });
     }
   })
 })
 
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 app.listen(3000, function(err) {
   if (err) {
     console.log(err);
